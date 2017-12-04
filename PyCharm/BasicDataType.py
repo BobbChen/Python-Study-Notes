977d01727d256d2b555c252974a78bc58cc52ff9
sggdsfd <chbsggdsfd@163.com>
2017-12-04 17:25:37 +0800

    初始化

---
+++ PyCharm/BasicDataType.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Filename : BasicDataType.py
+
+# python中的变量不需要声明
+counter = 100 # 整型
+miles = 100.0 # 浮点型
+name = "Bob" # 字符串
+d = e = f = 1, 2, "kim" # 创建一个实例对象，三个变量被分配到相同的内存空间
+a, b, c = 1, 2, "kim"  # 多个对象指定多个变量
+
+print(type(a),type(b),type(c)) # type()函数可以查询变量所指向的对象类型
+
+
+
+
+
+
+
---
+++ PyCharm/helloray.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Filename : helloray.py
+
+print ("Hello Python!") # Python中单行注释以#开头
+
+
+
